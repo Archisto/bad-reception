18cfe3c636f2a6878fe0b75158dde5c315e14528
Henri Sarasvirta <henri.sarasvirta@gmail.com>
2018-11-07 17:57:10 +0200

    Clamp käyttöön. Lisätty fontti.

--- Bad-reception/Assets/Scripts/RadioManager.cs
+++ Bad-reception/Assets/Scripts/RadioManager.cs
@@ -7,11 +7,16 @@ public class RadioManager : MonoBehaviour {
     public float minFrequency = 148.5f;
     public float maxFrequency = 283.5f;
 
-    public float frequency = 148.5f;
+    private float _frequency = 148.0f;
+    public float frequency
+    {
+        get{ return _frequency; }
+        set{ _frequency = Mathf.Clamp(value, minFrequency, maxFrequency); }
+    }
 
-	// Use this for initialization
-	void Start () {
-		
+    // Use this for initialization
+    void Start () {
+        
 	}
 	
 	// Update is called once per frame
