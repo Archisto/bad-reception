8821fdb7907feed8c1de7b22aeef15e3b0d1582f
Henri Sarasvirta <henri.sarasvirta@gmail.com>
2018-11-08 14:09:02 +0200

    kelloa

---
+++ Bad-reception/Assets/Scripts/Clock.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clock : MonoBehaviour {
+
+    public GameManager gameManager;
+    public GameObject minute;
+    public GameObject hour;
+
+    public float hours = 14f;
+    public float minutes = 37f;
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+
+	}
+}
